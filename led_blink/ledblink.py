eb13c0a3d83e0cb1821ffeb9b032c20d8a6d34b8
Matt Andrzejczuk <matt@jawn.it>
2019-03-07 12:38:04 -0500

    enable python3 compatibility
    slight adjustment for this script so that it can run on python3 and is also fully backwards compatible with python2.

--- led_blink/ledblink.py
+++ led_blink/ledblink.py
@@ -16,13 +16,13 @@ GPIO.setup(GREEN,GPIO.OUT)
 try:
 	while True:
 		#open led
-		print "open led"
+		print("open led")
 		GPIO.output(RED,GPIO.HIGH)
                 GPIO.output(YELLOW,GPIO.HIGH)
                 GPIO.output(GREEN,GPIO.HIGH)
 		time.sleep(0.5) #500ms
 		#close led
-		print "close led"
+		print("close led")
 		GPIO.output(RED,GPIO.LOW)
                 GPIO.output(YELLOW,GPIO.LOW)
                 GPIO.output(GREEN,GPIO.LOW)
